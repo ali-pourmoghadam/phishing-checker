cbe09a2b1902ab47048d3b7c331c953aa9c694dc
ali pourmoghadam <ali_pourmoghadam@protonmail.com>
2023-01-11 01:08:20 +0330

    feat : add qaulity score service and phishing contract

---
+++ app/Contracts/PhishingContract.php
@@ -0,0 +1,7 @@
+<?php
+
+interface PhishingContract {
+
+    public function urlCheck();
+
+}
---
+++ app/Services/QualityScoreService.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Services;
+
+use PhishingContract;
+
+class QualityScoreService implements PhishingContract{
+
+
+    public function __construct()
+    {
+       
+    }
+
+    public function urlCheck()
+    {
+        
+    }
+
+}
